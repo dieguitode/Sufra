31bf71545d9b9605acb26846dbdff9e78f18d67a
dieguitode <83051125+dieguitode@users.noreply.github.com>
2021-06-05 21:54:34 -0300

    Add files via upload

---
+++ programas/ejercicio3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+
+int main (){
+	int  primerVariable = 0;
+	int segundaVariable = 3;
+
+	printf (" segundavariable : %i - primervariable : %i", segundaVariable , primerVariable);
+	return 0;
+}
